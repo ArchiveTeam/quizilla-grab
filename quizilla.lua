c8944e8b6fab5d65bc04abbc993fa0e6e22487d0
Arkiver2 <Arkiver@hotmail.com>
2014-09-18 17:37:11 +0200

    quizilla.lua

--- quizilla.lua
+++ quizilla.lua
@@ -63,7 +63,7 @@ wget.callbacks.get_urls = function(file, url, is_css, iri)
           end
         end
       end
-      if string.match(html, 
+--      if string.match(html, 
         
     end
   end
