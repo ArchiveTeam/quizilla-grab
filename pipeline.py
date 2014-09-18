e2665c21be689c12d7d2bdc826d382be8fb635db
Arkiver2 <Arkiver@hotmail.com>
2014-09-18 23:02:55 +0200

    pipeline.py: add support for 'tag'

--- pipeline.py
+++ pipeline.py
@@ -198,7 +198,7 @@ class WgetArgs(object):
         item['item_type'] = item_type
         item['item_value'] = item_value
         
-        assert item_type in ('page', 'user')
+        assert item_type in ('page', 'user', 'tag')
 
         if item_type == 'page':
 #            wget_args.append('http://quizilla.teennick.com/')
@@ -218,7 +218,10 @@ class WgetArgs(object):
         
         elif item_type == 'user':
             wget_args.append('http://quizilla.teennick.com/user/{0}'.format(item_value))
-            
+        
+        elif item_type == 'tag':
+            wget_args.append('http://quizilla.teennick.com/tags/{0}'.format(item_value))
+        
         if 'bind_address' in globals():
             wget_args.extend(['--bind-address', globals()['bind_address']])
             print('')
