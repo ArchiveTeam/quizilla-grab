d2c1635df82a91edb585308a641914453163626b
Arkiver2 <Arkiver@hotmail.com>
2014-09-19 23:37:08 +0200

    quizilla.lua: add excludes

--- quizilla.lua
+++ quizilla.lua
@@ -32,6 +32,143 @@ local downloaded = {}
 local post_requests = {}
 local results = {}
 
+--exclude all these links
+downloaded["http://quizilla.teennick.com/templates/QZ2/images/dev/page_bg.gif"] = true
+downloaded["http://quizilla.teennick.com/templates/QZ2/images/dev/bg_continue_main.gif"] = true
+downloaded["http://quizilla.teennick.com/templates/QZ2/images/dev/bg_continue_right.gif"] = true
+downloaded["http://quizilla.teennick.com/templates/QZ2/images/dev/bg_continue_main.gif"] = true
+downloaded["http://quizilla.teennick.com/templates/QZ2/images/dev/feed-icon-14x14.png"] = true
+downloaded["http://quizilla.teennick.com/templates/QZ2/images/dev/icon_messages.gif"] = true
+downloaded["http://quizilla.teennick.com/templates/QZ2/images/dev/big_box.gif"] = true
+downloaded["http://quizilla.teennick.com/templates/QZ2/images/dev/628_header_bg_top.jpg"] = true
+downloaded["http://quizilla.teennick.com/templates/QZ2/images/dev/620_header_bg_top.jpg"] = true
+downloaded["http://quizilla.teennick.com/templates/QZ2/images/dev/628_header_bg_bottom.gif"] = true
+downloaded["http://quizilla.teennick.com/templates/QZ2/images/dev/620_header_bg_bottom.gif"] = true
+downloaded["http://quizilla.teennick.com/templates/QZ2/images/dev/302_header_bg_top.jpg"] = true
+downloaded["http://quizilla.teennick.com/templates/QZ2/images/dev/302_header_bg_bottom.gif"] = true
+downloaded["http://quizilla.teennick.com/templates/QZ2/images/dev/429_header_bg_top.jpg"] = true
+downloaded["http://quizilla.teennick.com/templates/QZ2/images/dev/429_header_bg_bottom.gif"] = true
+downloaded["http://quizilla.teennick.com/templates/QZ2/images/dev/qz_logo_sketchy.gif"] = true
+downloaded["http://quizilla.teennick.com/templates/QZ2/images/dev/bg_n_header.gif"] = true
+downloaded["http://quizilla.teennick.com/templates/QZ2/images/dev/nav_3state2.gif"] = true
+downloaded["http://quizilla.teennick.com/templates/QZ2/images/dev/bg_nav_hover_arrow.gif"] = true
+downloaded["http://quizilla.teennick.com/templates/QZ2/images/dev/bg_menu.gif"] = true
+downloaded["http://quizilla.teennick.com/templates/QZ2/images/dev/bg_nav_li.gif"] = true
+downloaded["http://quizilla.teennick.com/templates/QZ2/images/dev/bg_tabs_li.gif"] = true
+downloaded["http://quizilla.teennick.com/templates/QZ2/images/dev/bg_tabs_a.gif"] = true
+downloaded["http://quizilla.teennick.com/templates/QZ2/images/dev/bg_bottom_edge_rough.gif"] = true
+downloaded["http://quizilla.teennick.com/templates/QZ2/images/dev/bg_big_container_bottom.gif"] = true
+downloaded["http://quizilla.teennick.com/templates/QZ2/images/dev/big_box.gif"] = true
+downloaded["http://quizilla.teennick.com/templates/QZ2/images/dev/btn_go.gif"] = true
+downloaded["http://quizilla.teennick.com/templates/QZ2/images/dev/btn_cancel.gif"] = true
+downloaded["http://quizilla.teennick.com/templates/QZ2/images/dev/addimg.gif"] = true
+downloaded["http://quizilla.teennick.com/templates/QZ2/images/dev/btn_submit.gif"] = true
+downloaded["http://quizilla.teennick.com/templates/QZ2/images/dev/bg_bottom_edge_rough.gif"] = true
+downloaded["http://quizilla.teennick.com/templates/QZ2/images/dev/bg_arrow_sort.gif"] = true
+downloaded["http://quizilla.teennick.com/templates/QZ2/images/dev/bg_bottom_edge_rough.gif"] = true
+downloaded["http://quizilla.teennick.com/templates/QZ2/images/dev/bg_big_container_bottom.gif"] = true
+downloaded["http://quizilla.teennick.com/templates/QZ2/images/dev/bg_big_container_top.gif"] = true
+downloaded["http://quizilla.teennick.com/templates/QZ2/images/dev/bg_poll_result_bar.gif"] = true
+downloaded["http://quizilla.teennick.com/templates/QZ2/images/dev/bg_big_container_bottom.gif"] = true
+downloaded["http://quizilla.teennick.com/templates/QZ2/images/dev/bigger_box.gif"] = true
+downloaded["http://quizilla.teennick.com/templates/QZ2/images/dev/icon_email.gif"] = true
+downloaded["http://quizilla.teennick.com/templates/QZ2/images/dev/icon_yim.gif"] = true
+downloaded["http://quizilla.teennick.com/templates/QZ2/images/dev/icon_aim.gif"] = true
+downloaded["http://quizilla.teennick.com/templates/QZ2/images/dev/btn_tagstyle.gif"] = true
+downloaded["http://quizilla.teennick.com/templates/QZ2/images/dev/bg_tags_graph.gif"] = true
+downloaded["http://quizilla.teennick.com/templates/QZ2/images/dev/bg_bottom_edge_rough.gif"] = true
+downloaded["http://quizilla.teennick.com/templates/QZ2/images/dev/bg_arrow_sort.gif"] = true
+downloaded["http://quizilla.teennick.com/templates/QZ2/images/dev/icon_quiz_sm.gif"] = true
+downloaded["http://quizilla.teennick.com/templates/QZ2/images/dev/icon_test_sm.gif"] = true
+downloaded["http://quizilla.teennick.com/templates/QZ2/images/dev/icon_story_sm.gif"] = true
+downloaded["http://quizilla.teennick.com/templates/QZ2/images/dev/icon_poll_sm.gif"] = true
+downloaded["http://quizilla.teennick.com/templates/QZ2/images/dev/icon_poem_sm.gif"] = true
+downloaded["http://quizilla.teennick.com/templates/QZ2/images/dev/icon_lyrics_sm.gif"] = true
+downloaded["http://quizilla.teennick.com/templates/QZ2/images/dev/icon_quiz_locked.gif"] = true
+downloaded["http://quizilla.teennick.com/templates/QZ2/images/dev/icon_test_locked.gif"] = true
+downloaded["http://quizilla.teennick.com/templates/QZ2/images/dev/icon_story_locked.gif"] = true
+downloaded["http://quizilla.teennick.com/templates/QZ2/images/dev/icon_poll_locked.gif"] = true
+downloaded["http://quizilla.teennick.com/templates/QZ2/images/dev/icon_poem_locked.gif"] = true
+downloaded["http://quizilla.teennick.com/templates/QZ2/images/dev/icon_lyrics_locked.gif"] = true
+downloaded["http://quizilla.teennick.com/templates/QZ2/images/dev/delete.gif"] = true
+downloaded["http://quizilla.teennick.com/templates/QZ2/images/dev/star.gif"] = true
+downloaded["http://quizilla.teennick.com/templates/QZ2/images/dev/starrating.gif"] = true
+downloaded["http://quizilla.teennick.com/templates/QZ2/images/dev/bigger_box.gif"] = true
+downloaded["http://quizilla.teennick.com/templates/QZ2/images/dev/icon_help_question.gif"] = true
+downloaded["http://quizilla.teennick.com/templates/QZ2/images/dev/bg_bottom_edge_rough.gif"] = true
+downloaded["http://quizilla.teennick.com/templates/QZ2/images/dev/horoscopes/pisces.gif"] = true
+downloaded["http://quizilla.teennick.com/templates/QZ2/images/dev/horoscopes/aries.gif"] = true
+downloaded["http://quizilla.teennick.com/templates/QZ2/images/dev/horoscopes/taurus.gif"] = true
+downloaded["http://quizilla.teennick.com/templates/QZ2/images/dev/horoscopes/gemini.gif"] = true
+downloaded["http://quizilla.teennick.com/templates/QZ2/images/dev/horoscopes/cancer.gif"] = true
+downloaded["http://quizilla.teennick.com/templates/QZ2/images/dev/horoscopes/leo.gif"] = true
+downloaded["http://quizilla.teennick.com/templates/QZ2/images/dev/horoscopes/virgo.gif"] = true
+downloaded["http://quizilla.teennick.com/templates/QZ2/images/dev/horoscopes/libra.gif"] = true
+downloaded["http://quizilla.teennick.com/templates/QZ2/images/dev/horoscopes/scorpio.gif"] = true
+downloaded["http://quizilla.teennick.com/templates/QZ2/images/dev/horoscopes/sagittarius.gif"] = true
+downloaded["http://quizilla.teennick.com/templates/QZ2/images/dev/horoscopes/capricorn.gif"] = true
+downloaded["http://quizilla.teennick.com/templates/QZ2/images/dev/horoscopes/aquarius.gif"] = true
+downloaded["http://quizilla.teennick.com/templates/QZ2/images/dev/bg_bottom_edge_rough.gif"] = true
+downloaded["http://quizilla.teennick.com/templates/QZ2/images/dev/icon_closed.gif"] = true
+downloaded["http://quizilla.teennick.com/templates/QZ2/images/dev/icon_opened.gif"] = true
+downloaded["http://quizilla.teennick.com/templates/QZ2/images/dev/bg_friend_soundwave.gif"] = true
+downloaded["http://quizilla.teennick.com/templates/QZ2/images/dev/bg_bottom_edge_rough.gif"] = true
+downloaded["http://quizilla.teennick.com/templates/QZ2/images/dev/moods/angel.gif"] = true
+downloaded["http://quizilla.teennick.com/templates/QZ2/images/dev/moods/birthday.gif"] = true
+downloaded["http://quizilla.teennick.com/templates/QZ2/images/dev/moods/blush.gif"] = true
+downloaded["http://quizilla.teennick.com/templates/QZ2/images/dev/moods/brightidea.gif"] = true
+downloaded["http://quizilla.teennick.com/templates/QZ2/images/dev/moods/cheerful.gif"] = true
+downloaded["http://quizilla.teennick.com/templates/QZ2/images/dev/moods/clown.gif"] = true
+downloaded["http://quizilla.teennick.com/templates/QZ2/images/dev/moods/confused.gif"] = true
+downloaded["http://quizilla.teennick.com/templates/QZ2/images/dev/moods/cool.gif"] = true
+downloaded["http://quizilla.teennick.com/templates/QZ2/images/dev/moods/crying.gif"] = true
+downloaded["http://quizilla.teennick.com/templates/QZ2/images/dev/moods/evilgrin.gif"] = true
+downloaded["http://quizilla.teennick.com/templates/QZ2/images/dev/moods/excited.gif"] = true
+downloaded["http://quizilla.teennick.com/templates/QZ2/images/dev/moods/furious.gif"] = true
+downloaded["http://quizilla.teennick.com/templates/QZ2/images/dev/moods/heart.gif"] = true
+downloaded["http://quizilla.teennick.com/templates/QZ2/images/dev/moods/hungry.gif"] = true
+downloaded["http://quizilla.teennick.com/templates/QZ2/images/dev/moods/indifferent.gif"] = true
+downloaded["http://quizilla.teennick.com/templates/QZ2/images/dev/moods/love.gif"] = true
+downloaded["http://quizilla.teennick.com/templates/QZ2/images/dev/moods/mad.gif"] = true
+downloaded["http://quizilla.teennick.com/templates/QZ2/images/dev/moods/sad.gif"] = true
+downloaded["http://quizilla.teennick.com/templates/QZ2/images/dev/moods/scared.gif"] = true
+downloaded["http://quizilla.teennick.com/templates/QZ2/images/dev/moods/scaredtodeath.gif"] = true
+downloaded["http://quizilla.teennick.com/templates/QZ2/images/dev/moods/shocked.gif"] = true
+downloaded["http://quizilla.teennick.com/templates/QZ2/images/dev/moods/sick.gif"] = true
+downloaded["http://quizilla.teennick.com/templates/QZ2/images/dev/moods/sleepy.gif"] = true
+downloaded["http://quizilla.teennick.com/templates/QZ2/images/dev/moods/spin.gif"] = true
+downloaded["http://quizilla.teennick.com/templates/QZ2/images/dev/moods/tongue.gif"] = true
+downloaded["http://quizilla.teennick.com/templates/QZ2/images/dev/moods/upset.gif"] = true
+downloaded["http://quizilla.teennick.com/templates/QZ2/images/dev/moods/whistling.gif"] = true
+downloaded["http://quizilla.teennick.com/templates/QZ2/images/dev/moods/winking.gif"] = true
+downloaded["http://quizilla.teennick.com/templates/QZ2/images/dev/bg_bottom_edge_rough.gif"] = true
+downloaded["http://quizilla.teennick.com/templates/QZ2/images/dev/icon_closed.gif"] = true
+downloaded["http://quizilla.teennick.com/templates/QZ2/images/dev/icon_opened.gif"] = true
+downloaded["http://quizilla.teennick.com/templates/QZ2/images/dev/bg_bottom_edge_rough.gif"] = true
+downloaded["http://quizilla.teennick.com/templates/QZ2/css/indicator.gif"] = true
+downloaded["http://quizilla.teennick.com/templates/QZ2/images/dev/username.gif"] = true
+downloaded["http://quizilla.teennick.com/templates/QZ2/images/dev/password.gif"] = true
+downloaded["http://quizilla.teennick.com/templates/QZ2/images/dev/bg_fp_poll_result_bar280.gif"] = true
+downloaded["http://quizilla.teennick.com/templates/QZ2/images/dev/bg_bottom_edge_rough.gif"] = true
+downloaded["http://quizilla.teennick.com/templates/QZ2/images/dev/starrating.gif"] = true
+downloaded["http://quizilla.teennick.com/templates/QZ2/images/dev/bg_n_header.gif"] = true
+downloaded["http://quizilla.teennick.com/templates/QZ2/images/dev/n_logo_header.gif"] = true
+downloaded["http://quizilla.teennick.com/templates/QZ2/images/dev/628_header_bg_top.jpg"] = true
+downloaded["http://quizilla.teennick.com/templates/QZ2/images/dev/628_header_bg_bottom.gif"] = true
+downloaded["http://quizilla.teennick.com/templates/QZ2/images/dev/mb_tab_left.gif"] = true
+downloaded["http://quizilla.teennick.com/templates/QZ2/images/dev/mb_tab_right.gif"] = true
+downloaded["http://quizilla.teennick.com/templates/QZ2/images/dev/mb_topbar.gif"] = true
+downloaded["http://quizilla.teennick.com/templates/QZ2/images/dev/mb_bottombar.png"] = true
+downloaded["http://quizilla.teennick.com/templates/QZ2/images/dev/mb_bottombar_ie.gif"] = true
+downloaded["http://quizilla.teennick.com/templates/QZ2/images/dev/nick_footer_top.gif"] = true
+downloaded["http://quizilla.teennick.com/templates/QZ2/images/dev/nick_footer_bottom.gif"] = true
+downloaded["http://quizilla.teennick.com/templates/QZ2/images/dev/icon_footer_bullet.gif"] = true
+downloaded["http://quizilla.teennick.com/templates/QZ2/images/dev/footer_divider.gif"] = true
+downloaded["http://quizilla.teennick.com/templates/QZ2/images/dev/bg_wrapper.gif"] = true
+downloaded["http://quizilla.teennick.com/templates/QZ2/images/dev/macFFBgHack.png"] = true
+downloaded["http://cdn.gigya.com/wildfire/js/wfapiv2.js"] = true
+downloaded["http://quizilla.teennick.com/static/js/min/events-overlay-deps.js"] = true
+
 wget.callbacks.get_urls = function(file, url, is_css, iri)
   local urls = {}
   
@@ -55,7 +192,9 @@ wget.callbacks.get_urls = function(file, url, is_css, iri)
           or string.match(customurl, "/user_images/")
           or string.match(customurl, "/static/") then
           if downloaded[customurl] ~= true then
-            table.insert(urls, { url=customurl })
+            if not string.match(customurl, "teenninck") then
+              table.insert(urls, { url=customurl })
+            end
             if string.match(customurl, "www%.quizilla%.teennick%.com") then
               local customurlprocessed = string.gsub(customurl, "www%.quizilla%.teennick%.com", "quizilla%.teennick%.com")
               if downloaded[customurlprocessed] ~= true then
@@ -82,7 +221,9 @@ wget.callbacks.get_urls = function(file, url, is_css, iri)
           or string.match(customurl, "/user_images/")
           or string.match(customurl, "/static/") then
           if downloaded[customurl] ~= true then
-            table.insert(urls, { url=customurl })
+            if not string.match(customurl, "teenninck") then
+              table.insert(urls, { url=customurl })
+            end
           end
         end
       end 
@@ -246,6 +387,14 @@ wget.callbacks.download_child_p = function(urlpos, parent, depth, start_url_pars
     return false
   end
   
+  if string.match(url, "teenninck") then
+    return false
+  end
+  
+  if string.match(url, "http://quizilla%.teennick%.com/static/js/min/events%-overlay%-deps%.js") then
+    return false
+  end
+  
   if item_type == "page" then
     if string.match(url, item_value) then
       return true
@@ -351,6 +500,8 @@ wget.callbacks.httploop_result = function(url, err, http_stat)
     else
       return wget.actions.CONTINUE
     end
+  elseif status_code == 301 and string.match(url, "polls") then
+    return wget.actions.EXIT
   end
 
   tries = 0
